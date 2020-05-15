d46fe405a9045bd1b650f5e712d858665198df1b
Suzette <suzetteislam@gmail.com>
2020-05-14 23:59:03 -0400

    updates

--- frontend/src/Components/SuggestedRecipeCard.jsx
+++ frontend/src/Components/SuggestedRecipeCard.jsx
@@ -4,13 +4,15 @@ import {Link} from 'react-router-dom'
 const SuggestedRecipeCard = ({recipeName, imgSrc, id}) => {
     console.log(recipeName, imgSrc)
     return(
-        <Link to={`/recipe/${id}`}>
+        
         <div className='api-recipe' id={id} > 
         {/*  make a function that expands thumbnail to recipe page */ }
+        <Link to={`/recipe/${id}`}>
             <img src ={imgSrc} />
             <h1>{recipeName}</h1>
+            </Link>
         </div>
-        </Link>
+        
     )
 }
 
--- frontend/src/Components/users/UserDashboard.jsx
+++ frontend/src/Components/users/UserDashboard.jsx
@@ -14,7 +14,6 @@ const UserDashboard = ({ user }) => {
     //similar to component did mount
     useEffect(() => {
         populateRecipeArr();
-        // suggestedRecipeThumbnail()
         // getEvents();
     }, [])
 
